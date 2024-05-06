7bdf4757e26270d7261a17a4e150f44066d35df8
Jackson Harrison <Jackson.Harrison@stu.barren.kyschools.us>
2024-05-06 09:07:01 -0500

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Jackson Harrison on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
